4ff8634c46c1e72db67d03bc288489145426aceb
Lesly Juarez <leslyj2@gmail.com>
2020-12-03 16:15:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,17 @@
 require 'pry'
 
 class Artist
-  extend Findable
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
 
   @@artists = []
 
   def initialize
-    super 
+    super
     @songs = []
   end
 
