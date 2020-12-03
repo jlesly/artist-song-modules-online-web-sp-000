7b80043144ed3391d3b7339b3a6e58c4ad724503
Lesly Juarez <leslyj2@gmail.com>
2020-12-03 16:12:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,23 +29,3 @@ class Artist
   end
 
 end
-
-
-class Song
-  extend Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend Findable
-  include Paramable
-  attr_accessor :name
-  attr_reader :artist
-
-  @@songs = []
-
-  def artist=(artist)
-    @artist = artist
-  end
-
-  def self.all
-    @@songs
-  end
-end
